0cef1a09205d555ded704057794d19e1ca865999
qpfl117 <qpfl117@gmail.com>
2026-04-23 10:18:20 +0900

    update

--- update.bat
+++ update.bat
@@ -1,10 +1,12 @@
 @echo off
+chcp 65001 > nul
 set GAME_DIR=C:\Users\qpfl1\Desktop\GM\Wildtamerz
 set PUBLIC_DIR=%GAME_DIR%\public
 set DLDIR=C:\Users\qpfl1\Downloads
-if not exist "%DLDIR%\index.html" set DLDIR=C:\Users\qpfl1\ë¤ì´ë¡ë
 
-echo Checking: %DLDIR%
+echo ================================
+echo   WILD TAMERZ Auto Deploy
+echo ================================
 
 if exist "%DLDIR%\index.html" (
     echo [1/3] index.html found! Copying...
@@ -12,8 +14,8 @@ if exist "%DLDIR%\index.html" (
     del /F "%DLDIR%\index.html"
     echo [1/3] Done!
 ) else (
-    echo [ERROR] index.html not found!
-    echo Download index.html from Claude first.
+    echo [ERROR] index.html not found in Downloads!
+    echo Please download index.html from Claude first.
     pause
     exit
 )
@@ -28,4 +30,5 @@ echo [2/3] Done!
 echo [3/3] All done!
 echo Site will update in ~1 min:
 echo https://wild-tamerz.web.app
+echo ================================
 pause
